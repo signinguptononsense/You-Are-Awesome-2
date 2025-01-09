6e591d34701c85b5370c71d9e15f58f6c36b41d8
Seereus <signinguptononsense@gmail.com>
2025-01-09 03:14:18 +0000

    Initial Commit

---
+++ You Are Awesome 2/You_Are_Awesome_2App.swift
@@ -0,0 +1,17 @@
+//
+//  You_Are_Awesome_2App.swift
+//  You Are Awesome 2
+//
+//  Created by Colin on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct You_Are_Awesome_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
