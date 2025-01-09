45fda5ce0c23584dae2d90c1d5b76458f308b0e7
Seereus <signinguptononsense@gmail.com>
2025-01-09 03:33:31 +0000

    Added image

--- You Are Awesome 2/ContentView.swift
+++ You Are Awesome 2/ContentView.swift
@@ -9,18 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+              Image("swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.orange)
+            Text("You Are Awesome!")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.orange)
         }
         .padding()
+        
     }
+    
+    // New stuff here
 }
-// New Stuff
-
-
 #Preview {
-    ContentView()
 }
